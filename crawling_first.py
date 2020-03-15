cb00d0b4365bd28a7f82158079f6cac281f7b7a1
minjun.ha <haminjun0@gmail.com>
2020-03-15 13:05:44 +0900

    IMAX 영화 크롤링 처리 추가 및 인코딩 타입 utf-8 고정

--- crawling_first.py
+++ crawling_first.py
@@ -1,12 +1,25 @@
+#-*- coding: utf-8 -*-
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 import requests
 from bs4 import BeautifulSoup
 
-url = 'http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=01&theatercode=0013&date=20200315'
+url = 'http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=01&theatercode=0013&date=20200316'
 html = requests.get(url)
 
 # print(html.text)
 soup = BeautifulSoup(html.text, 'html.parser')
-title_list = soup.select('div.info-movie')
+imax = soup.select_one('span.imax')
+if(imax):
+    imax = imax.find_parent('div', class_='col-times')
+    title = imax.select_one('div.info-movie > a > strong').text.strip()
+    print(title + ' IMAX 예매가 열렸습니다.')
+else:
+    print('IMAX 예매가 아직 열리지 않았습니다.')
 
-for i in title_list:
-    print(i.select_one('a > strong').text.strip())
+# Parent to Child
+#title_list = soup.select('div.info-movie')
+#for i in title_list:
+#    print(i.select_one('a > strong').text.strip())
