8ad59aef6f04f971795376c03995c29dd16c0f02
minjun.ha <haminjun0@gmail.com>
2020-03-15 12:37:38 +0900

    기본 웹 리퀘스트 테스트 추가

---
+++ crawling_first.py
@@ -0,0 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=01&theatercode=0013&date=20200315'
+html = requests.get(url)
+print(html.text)
+
